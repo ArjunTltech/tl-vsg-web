9f874109195ab5cb21a6c6f9d96fadb212bc4bdf
tldasarath <tldasarath@gmail.com>
2025-05-14 18:32:33 +0530

    feat:Careers section updated

--- src/components/careers/CareerPageWithOpenings .js
+++ src/components/careers/CareerPageWithOpenings .js
@@ -46,15 +46,15 @@ const CareerPageWithOpenings = () => {
                                 {careers && careers.length > 0 ? (
                                     <>
                                         {"Email your resume, cover letter, and portfolio links with the position name in the subject line to: "}
-                                        <a href="mailto:careers@vsgenxsolutions.com" className={styles.emailLink}>
-                                            careers@vsgenxsolutions.com
+                                        <a href="mailto:enablement@vsgenxsolutions.com" className={styles.emailLink}>
+                                           enablement@vsgenxsolutions.com 
                                         </a>
                                     </>
                                 ) : (
                                     <>
                                         {"Even though we don't have current openings, we welcome you to send your resume for future opportunities to: "}
-                                        <a href="mailto:careers@vsgenxsolutions.com" className={styles.emailLink}>
-                                            careers@vsgenxsolutions.com
+                                        <a href="mailto:enablement@vsgenxsolutions.com" className={styles.emailLink}>
+                                            enablement@vsgenxsolutions.com 
                                         </a>
                                     </>
                                 )}
